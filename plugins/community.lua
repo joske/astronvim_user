20f84e1c6cb52def185b913ab20f645a27c2556d
Jos Dehaes <jos.dehaes@gmail.com>
2023-04-12 09:43:59 +0200

    add copilot/markdown

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,12 +5,13 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
 }
