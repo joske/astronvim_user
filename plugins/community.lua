ba70fb01c449024237838d7c62aebc1750fcad44
Jos Dehaes <jos.dehaes@gmail.com>
2023-04-15 20:50:23 +0200

    override rust-tools

--- plugins/community.lua
+++ plugins/community.lua
@@ -14,4 +14,31 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
+  {
+    "simrat39/rust-tools.nvim",
+    opts = {
+      tools = {
+        hover_actions = {
+          auto_focus = true,
+        },
+      },
+      server = {
+        on_attach = function(client, bufnr)
+          -- override here. call lsp on attach and then add own custom logic.
+          require("astronvim.utils.lsp").on_attach(client, bufnr)
+          local rt = require "rust-tools"
+
+          local utils = require "astronvim.utils"
+
+          utils.set_mappings({
+            n = {
+              ["<leader>r"] = { name = " Rust Tools" },
+              ["<leader>rr"] = { rt.hover_actions.hover_actions, desc = "Rust Hover Actions" },
+              ["<leader>ra"] = { rt.code_action_group.code_action_group, desc = "Rust Code Actions" },
+            },
+          }, { buffer = bufnr })
+        end,
+      },
+    },
+  },
 }
