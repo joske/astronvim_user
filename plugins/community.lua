705af37e49ff1882659fe5a8ae7414a80775fc4a
Jos Dehaes <jos.dehaes@gmail.com>
2023-04-16 22:14:44 +0200

    auto-enable crates

--- plugins/community.lua
+++ plugins/community.lua
@@ -14,4 +14,23 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
+  {
+    "Saecki/crates.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("BufRead", {
+        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+        pattern = "Cargo.toml",
+        callback = function()
+          require("cmp").setup.buffer { sources = { { name = "crates" } } }
+          require("crates").show()
+        end,
+      })
+    end,
+    opts = {
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
+      },
+    },
+  },
 }
