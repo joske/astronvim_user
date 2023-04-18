93780e9b7f01cad31fbbd9b6cc0f3cd027e3f632
Jos Dehaes <jos.dehaes@gmail.com>
2023-04-18 10:35:41 +0200

    add call hierarchy

--- init.lua
+++ init.lua
@@ -64,6 +64,10 @@ return {
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
     vim.cmd [[nnoremap <expr> <silent> 0 col('.') == match(getline('.'),'S')+1 ? '0' : '^']]
+    -- configure the litee.nvim library
+    require("litee.lib").setup {}
+    -- configure litee-calltree.nvim
+    require("litee.calltree").setup {}
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
